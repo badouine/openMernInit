d8ab9efb97274f4e5fa7fab4cdece6759810f08d
badouine <nzbadouine@gmail.com>
2022-04-11 04:35:53 +0000

    update file

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,7 +1,7 @@
 const Thing = require('../models/Thing');
 
 exports.createThing = (req, res, next) => {
-  
+
   const thingObject = JSON.parse(req.body.thing);
   delete thingObject._id;
   const thing = new Thing({
@@ -42,15 +42,13 @@ exports.getOneThing = (req, res, next) => {
   };
   
   exports.modifyThing = (req, res, next) => {
-    const thing = new Thing({
-      _id: req.params.id,
-      title: req.body.title,
-      description: req.body.description,
-      imageUrl: req.body.imageUrl,
-      price: req.body.price,
-      userId: req.body.userId
-    });
-    Thing.updateOne({_id: req.params.id}, thing).then(
+    const thingObject = req.file ?
+    {
+      ...JSON.parse(req.body.thing),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+      Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+      .then(
       () => {
         res.status(201).json({
           message: 'Thing updated successfully!'
